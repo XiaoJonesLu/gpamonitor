08fbb6af18f4d0c2a1abc2bfb47664c9ba4c44d2
Xiao Lu <Xiao@Xiaos-MacBook-Pro.local>
2016-10-22 12:28:58 +0400

    updated credential

--- includes/conf_server.php
+++ includes/conf_server.php
@@ -1,11 +1,8 @@
 <?php
 defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
 
-// local server, Xiao's Mac
+// local server
 defined('SITE_ROOT') ? null : define('SITE_ROOT', DS.'Applications'.DS.'MAMP'.DS.'htdocs'.DS.'gpamonitor');
-
-// remote server, Alicloud
-// defined('SITE_ROOT') ? null : define('SITE_ROOT', DS.'alidata'.DS.'www'.DS.'default'.DS.'gpamonitor');
 
 // remote server, AWS
 // defined('SITE_ROOT') ? null : define('SITE_ROOT', DS.'var'.DS.'www'.DS.'html'.DS.'server'.DS.'gpamonitor');
